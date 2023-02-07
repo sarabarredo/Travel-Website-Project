2e44c4c142c6f679225b9ccb6f69a904c5c3e25c
Sara Barredo <sarabarredo2000@gmail.com>
2023-02-07 19:02:04 +0100

    Primera versión

---
+++ script.js
@@ -0,0 +1,86 @@
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
+const textArray = ['Tokio.', 'Roma.', 'Bangkok.', 'Helsinki.', 'Lima.', 'Seúl.'];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if(!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursorSpan.classList.remove('typing');
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if(!cursorSpan.classList.contains('typing')) cursorSpan.classList.add('typing');
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursorSpan.classList.remove('typing');
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+// Abrir y cerrar el menu hamburguesa
+
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+// Formulario de contacto
+
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == '') {
+        parent.classList.remove('focus');
+    }
+    
+}
+
+inputs.forEach(inputs => {
+    inputs.addEventListener('focus', focusFunc);
+    inputs.addEventListener('blur', blurFunc);
+});
+
+
+// Cambiar imagen al seleccionar un destino
+
+document.getElementById('dest').onchange = function(event){  
+    document.getElementById('imagenCambiante').src = document.getElementById('dest').value;
+};
+
+
